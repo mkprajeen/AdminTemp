85da4d0666ab570c4225d4c4d9a38fad110a7133
PRAJEEN KUMAR <mkprajeen@hotmail.com>
2017-09-27 17:16:57 +0530

    Global state modified

--- src/app/global.state.ts
+++ src/app/global.state.ts
@@ -13,31 +13,31 @@ export class GlobalState {
     this._dataStream$.subscribe((data) => this._onEvent(data));
   }
 
-  notifyDataChanged(event, value) {
+  notifyDataChanged(key, value) {
 
-    let current = this._data[event];
+    let current = this._data[key];
     if (current !== value) {
-      this._data[event] = value;
+      this._data[key] = value;
 
       this._data.next({
-        event: event,
-        data: this._data[event]
+        Key: key,
+        Value: this._data[key]
       });
     }
   }
 
-  subscribe(event: string, callback: Function) {
-    let subscribers = this._subscriptions.get(event) || [];
+  subscribe(key: string, callback: Function) {
+    let subscribers = this._subscriptions.get(key) || [];
     subscribers.push(callback);
 
-    this._subscriptions.set(event, subscribers);
+    this._subscriptions.set(key, subscribers);
   }
 
-  _onEvent(data: any) {
-    let subscribers = this._subscriptions.get(data['event']) || [];
+  private _onEvent(data: any) {
+    let subscribers = this._subscriptions.get(data['Key']) || [];
 
     subscribers.forEach((callback) => {
-      callback.call(null, data['data']);
+      callback.call(null, data['Value']);
     });
   }
 }
