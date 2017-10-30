347038ea7a400a1cd828aeb2c7c07b50722fc09c
PRAJEEN KUMAR <mkprajeen@hotmail.com>
2017-10-30 14:16:11 +0530

    patient list server sorting

--- src/app/pages/patient/patient-list/patient-list.component.ts
+++ src/app/pages/patient/patient-list/patient-list.component.ts
@@ -13,21 +13,25 @@ export class PatientListComponent implements OnInit {
   rows = [];
   temp = [];
   selected = [];
+
   loadingIndicator: boolean = true;
   reorderable: boolean = true;
-  pageSize:number;
-  currentPage:number;
-  totalItems:number;
+  pageSize: number;
+  currentPage: number;
+  totalItems: number;
 
-   columns = [
-      { prop: 'PatientId' },
-      { prop: 'FirstName' },
-      { prop: 'LastName' }
-      ];
-  constructor( private _service:AuthenticationService ) {
-    this.currentPage=0;
-    this.pageSize =10;     
-   }
+  orderBy: string;
+  orderByDesc: boolean = false;
+
+  columns = [
+    { prop: 'PatientId', sortable: true },
+    { prop: 'FirstName', sortable: true },
+    { prop: 'LastName' }
+  ];
+  constructor(private _service: AuthenticationService) {
+    this.currentPage = 0;
+    this.pageSize = 10;
+  }
 
   ngOnInit() {
     // this._service.get(Global.BASE_TEMPLATE_ENDPOINT + 'patients')
@@ -36,15 +40,36 @@ export class PatientListComponent implements OnInit {
     // });
     this.setPage({ offset: 0 });
   }
-  setPage(pageInfo){
-    this.currentPage = pageInfo.offset ;
-
-    this._service.get(Global.BASE_TEMPLATE_ENDPOINT + 'patients/GetAllPagedPatients?page='+this.currentPage+'&pagesize='+this.pageSize  )
-    .subscribe(pagedData => {
-      this.rows=pagedData.Results;
-      this.totalItems = pagedData.TotalItems;
-    });
+  setPage(pageInfo) {
+    this.currentPage = pageInfo.offset;
+    var url;
+    if (this.orderBy) { 
+      if(this.orderByDesc){
+       url = Global.BASE_TEMPLATE_ENDPOINT + 'patients/GetAllPagedPatients?page=' + this.currentPage + '&pagesize=' + this.pageSize +'&orderBy='+ this.orderBy +'&orderByDesc=' + this.orderByDesc ;
+      }
+      else{
+        url = Global.BASE_TEMPLATE_ENDPOINT + 'patients/GetAllPagedPatients?page=' + this.currentPage + '&pagesize=' + this.pageSize +'&orderBy='+ this.orderBy ; 
+      }
+    }
+    else {
+       url = Global.BASE_TEMPLATE_ENDPOINT + 'patients/GetAllPagedPatients?page=' + this.currentPage + '&pagesize=' + this.pageSize;
+    }
+    this._service.get(url)
+      .subscribe(pagedData => {
+        this.rows = pagedData.Results;
+        this.totalItems = pagedData.TotalItems;
+        this.currentPage = pagedData.CurrentPage;
+      });
   }
- 
+  onSort(event) {
+    // event was triggered, start sort sequence
+    console.log('Sort Event', event);
+    const sort = event.sorts[0];
+    this.orderBy = sort.prop;
+    this.orderByDesc = sort.dir === 'desc' ? true : false;
+
+    this.setPage({ offset: this.currentPage } );
+  }
+
 
 }
