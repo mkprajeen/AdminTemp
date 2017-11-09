15affe2d32d680f3de62007a26888216305820fd
PRAJEEN KUMAR <mkprajeen@hotmail.com>
2017-11-09 21:45:43 +0530

    angualr 5 upgrade

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,5 +1,5 @@
 ﻿export class Global {
     public static BASE_TEMPLATE_ENDPOINT = 'http://localhost:51250/api/v1/';
-    //public static BASE_TEMPLATE_ENDPOINT = 'http://localhost:8090/api/';
+    //public static BASE_TEMPLATE_ENDPOINT = 'http://localhost:8090/api/v1/';
 
 }
